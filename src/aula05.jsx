83b73b9ff342048d1c9210635ad6a11140ca34e3
Laine2ds2025 <ville.ferreira@escola.pr.gov.br>
2025-08-07 08:36:39 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -1,37 +1,29 @@
-import React,{Component} from "react";
-import { View, Text, StyleSheet  } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-class Aula05 extends Component{
-    render(){
-        return(
-            <View style={ styles.container }>
-                <View style={ styles.caixa1 }>
-                    <Text style={{ color:'white'}}>Joice acompanhe a aula!</Text>
-                </View>
-                <View style={ styles.caixa2 }>2</View>
-                <View style={ styles.caixa3 }>3</View>
-            </View>
-                
-        );
-    }
+class Aula05  extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "Elaine"
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>My Component</Text>
+      </View>
+    );
+  }
 }
-export default Aula05;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-    },
-   caixa1: {
-    height: 50,
-    backgroundColor: 'red',
-   },
-   caixa2: {
+  container: {
     flex: 1,
-    backgroundColor: 'green',
-   },
-   caixa3: {
-    height: 50,
-    backgroundColor: 'blue',
-   }
-})
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default MyComponent;
